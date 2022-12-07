62fc6850a20f41e6e9c352f30fd2046026a16c83
Regor Carlo <esconderegor@gmail.com>
2022-12-07 11:44:31 +0800

    added new tab function to social icons

--- client/src/components/SocialIcons/Socials.jsx
+++ client/src/components/SocialIcons/Socials.jsx
@@ -10,10 +10,13 @@ export default function Socials() {
       <a href="http://twitter.com/jaregor" target="blank">
         <AiFillTwitterCircle />
       </a>
-      <a href="https://www.linkedin.com/in/regor-carlo-esconde-55570a195/">
+      <a
+        href="https://www.linkedin.com/in/regor-carlo-esconde-55570a195/"
+        target="blank"
+      >
         <AiFillLinkedin />
       </a>
-      <a href="https://github.com/13urning">
+      <a href="https://github.com/13urning" target="blank">
         <AiFillGithub />
       </a>
     </div>
